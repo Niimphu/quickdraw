404581c59b71d0f3824b965da97f705efb7bdf30
Niimphu <nimphu@outlook.com>
2026-04-14 19:54:55 +0100

    updated reticle

--- reticle.gd
+++ reticle.gd
@@ -1,11 +1,12 @@
 extends TextureRect
 
-
-func _ready() -> void:
+var centre_dot = load("res://img/centredot.png")
+func _ready():
+	Input.set_custom_mouse_cursor(centre_dot, 0, Vector2(4, 4))
 	EventBus.accuracy_modifier_changed.connect(_on_accuracy_modifier_changed)
 
 
-func _physics_process(_delta: float) -> void:
+func _process(_delta: float) -> void:
 	position = get_viewport().get_mouse_position() - (size * scale / 2)
 
 
--- test.gd
+++ test.gd
@@ -1,8 +1,3 @@
 extends Node2D
 
 @export var Player: CharacterBody2D
-
-var reticle = load("res://img/mscrosshair.png")
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
