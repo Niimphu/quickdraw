0711f0b2c672800097b24a59568b83d731b2728b
Niimphu <nimphu@outlook.com>
2026-05-01 12:57:40 +0100

    ammo work

--- ammo_indicator.gd
+++ ammo_indicator.gd
@@ -3,16 +3,28 @@ extends TextureProgressBar
 @export var MaskBar: ProgressBar
 @export var ChargedBullets: TextureProgressBar
 
+var current_ammo: int = 6
+var current_charged: int = 0
+var empty_ammo: int = 0
+
 
 func _ready() -> void:
 	EventBus.ammo_changed.connect(update_ammo)
 	EventBus.charged_bullet.connect(update_charged_bullets)
-
+	
 
 func update_ammo(new_value: int) -> void:
+	current_ammo = new_value
+	empty_ammo = 6 - current_ammo
 	value = new_value
-	MaskBar.value = new_value
+	update()
 
 
 func update_charged_bullets(new_value: int) -> void:
-	ChargedBullets.value = new_value
+	current_charged = new_value
+	update()
+
+
+func update() -> void:
+	ChargedBullets.value = current_ammo
+	MaskBar.value = current_charged + empty_ammo
